df02202a1b48425748fc583f212e21a77534d420
hippoDev <righthun.kwon@gmail.com>
2023-07-08 17:57:36 +0900

    2023.07.08 level1 & level2

---
+++ Baekjoon/level1/P10869.py
@@ -0,0 +1,6 @@
+A, B = map(int, input().split())
+print(A + B)
+print(A - B)
+print(A * B)
+print(A // B) # 몫 구하기 : //
+print(A % B)
---
+++ Baekjoon/level1/P2588.py
@@ -0,0 +1,8 @@
+# 문자열의 인덱스를 활용한다
+A = int(input())
+B = input()
+
+print(A * int(B[2]))
+print(A * int(B[1]))
+print(A * int(B[0]))
+print(A * int(B))
